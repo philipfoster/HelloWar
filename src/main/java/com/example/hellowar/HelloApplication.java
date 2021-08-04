83a13bf207774aa10dd399cd94a68e2e62ad8904
Philip <pfoster@redhat.com>
2021-08-04 16:15:46 -0500

    New project

---
+++ src/main/java/com/example/hellowar/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.hellowar;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
